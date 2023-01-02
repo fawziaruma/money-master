4437b6bde775235a81b918ce9b4dd7cdd21231ba
Fawzia Ruma <fawziaruma0652@gmail.com>
2023-01-02 20:43:42 +0600

    last commit

--- script.js
+++ script.js
@@ -1,31 +1,69 @@
+function mainBalance(){
+    const foodValue = document.getElementById('food').value 
+    const rentValue = document.getElementById('rent').value 
+    const clothesValue = document.getElementById('clothes').value 
+    let totalExpenses = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothesValue)
+    const inputIncome = document.getElementById('income-input').value
+    const mainBalance = parseInt(inputIncome) - totalExpenses
+    return mainBalance
+}
+
 document.getElementById('calculate-button').addEventListener('click', function(){
-
-    const food = document.getElementById('food')
-    const foodText = food.value 
-    const foodAmount = parseFloat(foodText)
-
-    const rent = document.getElementById('rent')
-    const rentText = rent.value 
-    const rentAmount = parseFloat(rentText)
-
-    const clothes = document.getElementById('clothes')
-    const clothesText = clothes.value 
-    const clothesAmount = parseFloat(clothesText)
-
-    const newAmount = foodAmount + rentAmount + clothesAmount ;
-
-    const incomeInput = document.getElementById('income-input')
-    const newIncomeText = incomeInput.value 
-    const newIncomeAmount = parseFloat(newIncomeText)
-    const updateAmount = newIncomeAmount - newAmount
-
-    const totalAmount = document.getElementById('total')
-    const totalAmountText = totalAmount.innerText
-    const previousTotal = parseFloat(totalAmountText)
-    const newTotal = previousTotal + updateAmount
-    totalAmount.innerText = newTotal
-
-
-
+    //.....total expenses........//
+    const foodValue = document.getElementById('food').value 
+    const rentValue = document.getElementById('rent').value 
+    const clothesValue = document.getElementById('clothes').value 
+    let totalExpenses = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothesValue)
+    console.log(totalExpenses);
+    if( isNaN != ( totalExpenses ) && (totalExpenses > 0)){
+        const inputExpenses = document.getElementById('total').innerText
+        document.getElementById('total').innerText = totalExpenses
+    }
+    else{
+        alert('please input valid number')
+    }
+    // ......total income....//
+    const inputIncome = document.getElementById('income-input').value
     
+    if(isNaN != (inputIncome) && (inputIncome > 0)){
+     const totalBalance = mainBalance()
+     if(inputIncome > totalExpenses){
+        document.getElementById('balance').innerText = totalBalance
+     }
+     else{
+         alert('it is  not  valid')
+     }
+
+    }
+    else{
+        alert('please input valid number')
+    }
+
+})
+ // save button 
+document.getElementById('btn').addEventListener('click', function(){
+    const totalBalance = mainBalance()
+
+
+    const saveInput = document.getElementById('save').value
+    const totalSave = (totalBalance * parseInt(saveInput) ) / 100 ;
+    const savingAmount = document.getElementById('saving-amount').innerText
+    if(totalBalance > totalSave){
+        document.getElementById('saving-amount').innerText = totalSave
+        
+    }
+    else{
+        alert('its not valid')
+        return
+    }
+
+    // ...............//
+    const newSave = totalBalance - totalSave ;
+    console.log(newSave);
+    const newSavingAmount = document.getElementById('remaining-balance').innerText
+    document.getElementById('remaining-balance').innerText = newSave
+
+
+
+
 })
